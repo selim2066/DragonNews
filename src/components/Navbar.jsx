ceb0c8c4ff34e93dc9b33ab463ae35b7724a93f4
selim2066 <mdselimreza2066@gmail.com>
2025-10-05 17:48:24 +0600

    dynamic login logout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   const {user}=useContext(AuthContext)
   return (
     <div className="flex justify-between items-center py-7 px-3">
-      <div>{user && user.name}</div>
+      <div>{user && user.email}</div>
       <div className="space-x-5">
         <Link to="/">Home</Link>
         <Link to="/carrer">Career</Link>
@@ -15,7 +15,10 @@ const Navbar = () => {
       </div>
       <div className="flex gap-2 items-center">
         <img src={userIcon} alt="" />
-        <Link to="/auth/login" className="btn bg-black/50 text-white font-bold">Login</Link>
+        
+        {
+          user && user?.email? <button  className="btn bg-black/50 text-white font-bold">Log-Out</button> :<Link to="/auth/login" className="btn bg-black/50 text-white font-bold">Login</Link>
+        }
       </div>
     </div>
   );
