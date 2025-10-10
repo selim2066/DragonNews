772797c8ed9c69d88d5902e487930dbe7f1372b6
selim2066 <mdselimreza2066@gmail.com>
2025-10-10 22:12:53 +0600

    file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <div className="carousel carousel-vertical rounded-box h-96">
-  <div className="carousel-item h-full">
-    <img src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp" />
-  </div>
-  <div className="carousel-item h-full">
-    <img src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp" />
-  </div>
-  <div className="carousel-item h-full">
-    <img src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp" />
-  </div>
-  <div className="carousel-item h-full">
-    <img src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.webp" />
-  </div>
-  <div className="carousel-item h-full">
-    <img src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp" />
-  </div>
-  <div className="carousel-item h-full">
-    <img src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp" />
-  </div>
-  <div className="carousel-item h-full">
-    <img src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp" />
-  </div>
-</div>
-      
+     
     </>
-  )
+  );
 }
 
-export default App
+export default App;
